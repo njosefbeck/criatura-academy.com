5f796aad2bc09f6f2a7e6dd81d7dc28f29cfec12
Nathan Beck <njosefbeck@gmail.com>
2020-07-09 21:20:29 -0500

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+const htmlmin = require("html-minifier");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false);
+
+  eleventyConfig.addWatchTarget("./_tmp/index.css");
+
+  eleventyConfig.addPassthroughCopy({ "./_tmp/index.css": "./css/index.css" });
+
+  eleventyConfig.addShortcode("version", function () {
+    return String(Date.now());
+  });
+
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith(".html")
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Criatura Academy
+
+Fan site!
