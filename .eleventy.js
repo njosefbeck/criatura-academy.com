adfa57a079433200ec3d3fef064e53204b691ae1
Nathan Beck <njosefbeck@gmail.com>
2020-09-18 15:51:03 -0500

    Add newsletter signup

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
 
   eleventyConfig.addWatchTarget("./_tmp/index.css");
-
   eleventyConfig.addPassthroughCopy({ "./_tmp/index.css": "./css/index.css" });
 
   eleventyConfig.addShortcode("version", function () {
